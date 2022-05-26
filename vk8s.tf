a9550f17e5ed25aaa8e258f584889c9b7b16c826
Marcel Wiget <mwiget@gmail.com>
2022-05-26 09:14:30 +0000

    all re virtual site

--- vk8s.tf
+++ vk8s.tf
@@ -1,11 +1,16 @@
 resource "volterra_virtual_k8s" "cluster" {
     name      = format("%s-vk8s", var.projectPrefix)
+    vsite_refs {
+      name      = "ves-io-all-res"
+      namespace = "shared"
+      tenant    = "ves-io"
+    }
     namespace = var.namespace
     depends_on = [ volterra_namespace.ns ]
 }
 
 resource "volterra_api_credential" "cluster" {
-  name      = "cluster-kubeconfig"
+  name      = format("%s-vk8s-kubeconfig", var.projectPrefix)
   api_credential_type = "KUBE_CONFIG"
   virtual_k8s_namespace = var.namespace
   virtual_k8s_name = volterra_virtual_k8s.cluster.name
@@ -15,3 +20,4 @@ resource "local_file" "kubeconfig" {
     content  = base64decode(volterra_api_credential.cluster.data)
     filename = "/home/gitpod/.kube/config"
 }
+
