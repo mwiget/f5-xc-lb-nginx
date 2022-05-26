12f3ba1bd187059764697d1347d9935e21ddc02f
Marcel Wiget <mwiget@gmail.com>
2022-05-26 07:54:03 +0000

    create kubeconfig

--- vk8s.tf
+++ vk8s.tf
@@ -3,3 +3,15 @@ resource "volterra_virtual_k8s" "cluster" {
     namespace = var.namespace
     depends_on = [ volterra_namespace.ns ]
 }
+
+resource "volterra_api_credential" "cluster" {
+  name      = "cluster-kubeconfig"
+  api_credential_type = "KUBE_CONFIG"
+  virtual_k8s_namespace = var.namespace
+  virtual_k8s_name = volterra_virtual_k8s.cluster.name
+}
+
+resource "local_file" "kubeconfig" {
+    content  = base64decode(volterra_api_credential.cluster.data)
+    filename = "/home/gitpod/.kube/config"
+}
